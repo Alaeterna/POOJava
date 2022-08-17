5e73c3a2d6619a999bbf1440f0df210f6d0fd91d
Einer Aponte <eerywolf@gmail.com>
2022-08-17 12:25:10 -0500

    Curso de POO en java

---
+++ src/Metodos/mainAngulo.java
@@ -0,0 +1,16 @@
+package Metodos;
+
+
+public class mainAngulo {
+    public static void main(String[] args) {
+        
+        //Creando Objeto
+        Angulo angulo = new Angulo();
+        
+        //Instanciando los metodos
+        angulo.entradaNumero();
+        angulo.calcularSeno();
+        angulo.calcularCoceno();
+        angulo.mostrarResultados();
+    }
+}
